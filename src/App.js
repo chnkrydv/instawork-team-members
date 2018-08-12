8562f660f238058ae72e75a262f91c42236698d7
unknown <chandank@triconinfotech.com>
2018-08-12 19:35:25 +0530

    empty master branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
-import { applyMiddleware, createStore } from 'redux';
-import { Provider } from 'react-redux';
-import logger from 'redux-logger';
 import './app.css';
-
-import rootReducer from './store/reducers/';
-import Root from './view/Root';
-
-const store = createStore(rootReducer, applyMiddleware(logger));
 
 const App = () => {
     return (
-      <Provider store={store}>
-        <Root />
-      </Provider>
+      <div>empty master branch</div>
     );
 }
 
